af6496ff55f17bc15adddf53b927430f8556c294
Katie R-A <kreidanderson@msn.com>
2020-10-14 14:50:23 -0500

    "adding all files to github"

---
+++ index.js
@@ -0,0 +1,20 @@
+const generateMarkdown = require('utils`/generateMarkdown')
+
+// array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+    generateMarkdown(data)
+}
+
+// function to initialize program
+function init() {
+// ask the inquirer stuff
+// one question asks about filename
+}
+
+// function call to initialize program
+init();
